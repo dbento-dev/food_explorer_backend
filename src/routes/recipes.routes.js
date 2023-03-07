1ae6f227dac05f11a5c93aa6b3ebb36999df3c1c
David Bento <dbento.dev@gmail.com>
2023-03-07 20:58:29 -0300

    adjust recipes upload photo

--- src/routes/recipes.routes.js
+++ src/routes/recipes.routes.js
@@ -6,11 +6,21 @@ const recipesController = new RecipesController()
 
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
+
+const upload = multer(uploadConfig.MULTER)
+
 recipesRoutes.use(ensureAuthenticated)
 
 recipesRoutes.post('/', recipesController.create)
 recipesRoutes.get('/:id', recipesController.show)
 recipesRoutes.delete('/:id', recipesController.delete)
 recipesRoutes.get('/', recipesController.index)
+
+recipesRoutes.patch('/avatar', upload.single('image'), (req, res) => {
+  console.log(req.file.filename)
+  res.json()
+})
 
 module.exports = recipesRoutes
