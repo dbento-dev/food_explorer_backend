dc1e513de02407fcb167a8fdbb5f5c0dd4007725
David Bento <dbento.dev@gmail.com>
2023-03-07 20:41:19 -0300

    create disk storage function

---
+++ src/configs/upload.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const multer = require('multer')
+const crypto = require('crypto')
+
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
+const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads')
+
+const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename: (req, file, cb) => {
+      const fileHash = crypto.randomBytes(16).toString('hex')
+      const fileName = `${fileHash}-${file.originalname}`
+
+      return cb(null, fileName)
+    }
+  })
+}
+
+module.exports = {
+  MULTER,
+  TMP_FOLDER,
+  UPLOADS_FOLDER
+}
