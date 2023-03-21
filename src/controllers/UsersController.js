c98b8d330a5e4cdb1e5eadaf47468df069f48a9a
David Bento <dbento.dev@gmail.com>
2023-03-21 10:40:10 -0300

    add admin scope to users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -37,7 +37,7 @@ class UsersController {
   async update(req, res) {
     // const { id } = req.params
     const user_id = req.user.id
-    const { name, email, password, old_password } = req.body
+    const { name, email, password, old_password, is_admin } = req.body
 
     const db = await sqliteConnection()
 
@@ -58,6 +58,7 @@ class UsersController {
 
     user.name = name ?? user.name
     user.email = email ?? user.email
+    user.is_admin = is_admin ?? user.is_admin
 
     if (password && !old_password) {
       throw new AppError(
@@ -77,8 +78,8 @@ class UsersController {
     }
 
     await db.run(
-      `UPDATE users SET name = (?), email = (?), password = (?), updated_at = DATETIME('now') WHERE id = (?)`,
-      [user.name, user.email, user.password, user_id]
+      `UPDATE users SET name = (?), email = (?), password = (?), is_admin = (?), updated_at = DATETIME('now') WHERE id = (?)`,
+      [user.name, user.email, user.password, user.is_admin, user_id]
     )
 
     return res.status(200).json({ message: 'User updated successfully' })
